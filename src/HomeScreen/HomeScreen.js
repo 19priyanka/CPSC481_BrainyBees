69d4099bd86fce975274f69e9a4a9be62ef04db4
Abigia Debebe <Abitibebu123@gmail.com>
2023-11-28 23:04:26 -0700

    Edited the homepage

--- src/HomeScreen/HomeScreen.js
+++ src/HomeScreen/HomeScreen.js
@@ -1,28 +1,73 @@
 import * as React from "react";
 import { useNavigate } from "react-router-dom";
-import {Button} from '@mui/material';
-import GenericSelectionPage from "../GenericSelectionPage.jsx";
+import { Button } from "@mui/material";
+import Homescreenselection from "./Homescreenselection";
+
 function HomeScreen(props) {
   const navigate = useNavigate();
   //change this later, just an example right now
   //change this later, just an example right now
   //change this later, just an example right now
   const links = [
-    { link: "/Lessons", name: "Lessons" },
-    { link: "/PracticeProblems", name: "Practice Problems" },
-    { link: "/Quizzes", name: "Quiz Yourself" },
-    { link: "/games", name: "Games" },
-    { link: "/supplmentarydocs", name: "Supplementary Docs" },
+    {
+      link: "/Lessons",
+      name: "Lessons",
+      desc: "Learn some coding using videos and reading material!",
+      image:
+        "https://images.squarespace-cdn.com/content/v1/570ebc54e32140a560e3d6d4/1610738838099-F36MYQU07CGHTKXFP941/codieclear.png?format=1500w",
+      progress: "25%",
+    },
+    {
+      link: "/PracticeProblems",
+      name: "Practice Problems",
+      desc: "Practice what you've learnt and get some in-depth solution walkthroughs!",
+      image:
+        "https://thumbs.dreamstime.com/z/retro-illustration-style-cartoon-test-paper-creative-illustrated-147659059.jpg?w=768",
+      progress: "30%",
+    },
+    {
+      link: "/Quizzes",
+      name: "Quizzes",
+      desc: "Test how well you've understood the lessons by taking some timed quizzes!",
+      image:
+        "https://img.freepik.com/premium-vector/cartoon-red-ringing-alarm-clock_81894-2843.jpg?w=740",
+      progress: "35%",
+    },
+    {
+      link: "/games",
+      name: "Games",
+      desc: "Practice some coding concepts by playing some fun interactive games!",
+      image:
+        "https://previews.123rf.com/images/thaiprayboy/thaiprayboy1901/thaiprayboy190100063/126054904-arcade-game-controller-joystick-on-isolated-white-background-cartoon-style-vector.jpg",
+      progress: "5%",
+    },
+    {
+      link: "/supplmentarydocs",
+      name: "Supplementary Docs",
+      desc: "Access some extra resources to help you visualize and understand the lessons better!",
+      image:
+        "https://as1.ftcdn.net/v2/jpg/01/20/65/30/1000_F_120653052_ECItG7Cc46JZJ46B0ABlcpOLUylOnJUs.jpg",
+    },
+    {
+      link: "/supplmentarydocs",
+      name: "Challenge Problems",
+      desc: "Think you're ready for a black belt? Click here for some hardcore challenges!",
+      image:
+        "https://img.freepik.com/free-vector/girl-thinking-with-question-mark-callouts_1308-94466.jpg?w=740&t=st=1701231992~exp=1701232592~hmac=804fb5030bd19c07532d508606efb2e42ec9c4b3e6936ea2880f5efe15763971",
+      progress: "15%",
+    },
   ];
   return (
-
     <div style={{ marginTop: "1em" }}>
-     <Button
+      <Button
         variant="outlined"
         color="primary"
         startIcon={<span>&#8592;</span>}
-         onClick={()=>navigate("/")}>Back To Landing Page </Button>
-      <GenericSelectionPage
+        onClick={() => navigate("/")}
+      >
+        Back To Landing Page{" "}
+      </Button>
+      <Homescreenselection
         links={links}
         grid={true}
         handleLanguageChange={(value) => {
@@ -30,7 +75,6 @@ function HomeScreen(props) {
         }}
         title="Learn All About "
       />
-      
     </div>
   );
 }
--- src/Quiz/QuizPage.js
+++ src/Quiz/QuizPage.js
@@ -1,42 +1,16 @@
 import React from "react";
 import Genericquestionsection from "./Genericquestionsection";
 import ShowPath from "../Showpath";
+import Questions from "./Questions";
 
 export default function QuizPage() {
-  const loopQns = [
-    {
-      question: "What does a 'for loop' do in C++ programming?",
-      choice1: "Tells the computer to stop",
-      choice2: "Repeats a set of instructions a specific number of times",
-      choice3: " Displays a message on the screen",
-    },
-    {
-      question: "How do you write a 'for loop' in C++?",
-      choice1: "'if (condition) { /* code here */ }'",
-      choice2: "'while (condition) { /* code here */ }'",
-      choice3:
-        "'for (initialization; condition; increment) { /* code here */ }'",
-    },
-    {
-      question:
-        "How many times will this 'for loop' repeat in C++: for (int i = 0; i < 5; i++) { /* code here */ }",
-      choice1: "5 times",
-      choice2: "4 times",
-      choice3: "6 times",
-    },
-    {
-      question:
-        "What is the result of this 'for loop' in C++: for (int i = 0; i < 3; i++) { cout << 'Hello'; }",
-      choice1: "'Hello'",
-      choice2: "'HelloHello'",
-      choice3: "'Hello' printed 3 times",
-    },
-  ];
+  const loopQns = Questions[0];
   return (
     <>
       <ShowPath></ShowPath>
-      <h1 style={{ color: "blue" }}>LOOPS</h1>
-      <Genericquestionsection questions={loopQns} />;
+      <section className="mainSection mx-4 mb-4">
+        <Genericquestionsection questions={loopQns} title={"LOOPS"} />;
+      </section>
     </>
   );
 }
