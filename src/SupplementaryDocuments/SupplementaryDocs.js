4f3710eef356116a4b49775db54a31716d539bc3
C.Kalin <87056237+10162051@users.noreply.github.com>
2023-12-03 19:03:36 -0700

    Formatting and image updates.

--- src/SupplementaryDocuments/SupplementaryDocs.js
+++ src/SupplementaryDocuments/SupplementaryDocs.js
@@ -1,45 +1,65 @@
 // SupplementaryDocs.js
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from '@mui/material/Button';
 import ForLoop from './ForLoop.png';
+import ForLoop2 from './ForLoop2.jpg';
 import './SupplementaryDocs.css';
-import ShowPath from "../Showpath.jsx";
+import ShowPath from '../Showpath.jsx';
 
 function SupplementaryDocs() {
   const navigate = useNavigate();
 
   return (
     <div className="supplementary-docs">
-       <ShowPath></ShowPath>
       <header>
-        <div className="top-center">
-          <h1 style={{ color: 'black' }}>Supplementary Documents: For Loops</h1>
+        <div className="top-left">
+          <ShowPath className="show-path" />
         </div>
       </header>
       <main className="text-image-container">
-        <div className="image-section">
-          <img src={ForLoop} alt="ForLoop" />
-        </div>
-        <div className="text-section">
-          <p>A FOR LOOP asks us to do something a certain number of times. For example, I want to buy 3 balloons. 
-            Instead of writing the same lines of code 3 times, I can write 1 loop and it does all the work for me!
-            In this example, we start with 0 balloons. I then check how many I've bought. Since this is the very beginning, I haven't bought any yet.
-            I go to the loop and buy one. Now I have 1 balloon. 
-            The number of balloons I have has been INCREMENTED or increased by 1.
-            Then I check again; how many balloons do I have?
-            Since I want 3 balloons, but only have 1, I'm going to do the loop again and buy another balloon. My code INCREMENTS
-            again so now I have 2 balloons. 2 is still not enough, so I'm going to do the loop again. Now that I've bought 
-            another balloon, I have 3! Since I have 3 balloons now, the loop is done and I can do something else.
-          </p>
+        <h1 style={{ color: 'black', textAlign: 'left' }}>Supplementary Documents: For Loops</h1>
+        <div className="content">
+          <div className="image-section">
+            <img src={ForLoop} alt="ForLoop" />
+            <img src={ForLoop2} alt="ForLoop2" />
+          </div>
+          <div className="text-section">
+            <ol>
+              <li>
+                <strong>Keyword:</strong> <code>for</code>
+                <p>This tells the computer what kind of loop we want. There are many different ones to choose from.</p>
+              </li>
+              <li>
+                <strong>Control Variable:</strong> <code>counter</code>
+                <p>This makes a variable called 'counter' so we can keep track of how many balloons we want to buy.</p>
+              </li>
+              <li>
+                <strong>Initial Value:</strong> 1
+                <p>How many balloons do I have before I do the loop</p>
+              </li>
+              <li>
+                <strong>Loop Condition:</strong> <code>counter &lt;= 5</code>
+                <ul>
+                  <li>This tells me how many times the loop is going to run.</li>
+                  <li><code>&lt;=</code> means "less than or equal to". We want the loop to run as long as the number is less than or equal to 5.</li>
+                </ul>
+              </li>
+              <li>
+                <strong>Increment counter variable:</strong> <code>counter++</code>
+                <ul>
+                  <li>This adds 1 to the counter each time the loop runs.</li>
+                  <li>The <code>++</code> part is what makes the counter change.</li>
+                  <li>We could also DECREMENT (make the counter go down instead of up) by using <code>counter--</code> too!</li>
+                </ul>
+              </li>
+            </ol>
+          </div>
         </div>
       </main>
       <div className="bottom-left">
-       
+        {/* Additional content */}
       </div>
     </div>
   );
 }
-
 export default SupplementaryDocs;
-
