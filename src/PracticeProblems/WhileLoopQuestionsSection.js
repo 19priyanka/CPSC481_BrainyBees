adf5d37252f87c48234a2ac19ab3d84988379ba5
C.Kalin <87056237+10162051@users.noreply.github.com>
2023-12-03 22:49:18 -0700

    Updated text

--- src/PracticeProblems/WhileLoopQuestionsSection.js
+++ src/PracticeProblems/WhileLoopQuestionsSection.js
@@ -95,7 +95,7 @@ int main() {
         <br/>
         [c] There is 38
         <br/>
-        [c] There is 37
+        [d] There is 37
          </div>),
       correctAnswer: (<div><br/>There are 38 MARVEL superheroes, now try to name them all!</div>),
       image: superh,
